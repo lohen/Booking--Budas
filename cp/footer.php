5482a0a61260d040b76129883954274e8c949438
lohen <lohen.carlos@gmail.com>
2015-11-21 13:48:01 -0200

    1

---
+++ cp/footer.php
@@ -0,0 +1,10 @@
+</div>
+<div class="clr"></div>
+</div>
+</div>
+<div class="clr"></div>
+<div id="footer">
+ <p class="fl-left">Hotel Booking System v2.0</p>
+ <p class="fl-right">© 2008 - 2012 Copyright <a href="http://www.bestsoftinc.com" target="_blank" style="color:#CCC;">Best Soft Inc</a> </p>
+</div>
+</body></html>
